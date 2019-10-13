f44a5815e262e02e52c6d21837f53ed601e19ac1
Kostas <konpap94@bu.edu>
2019-10-13 19:54:09 -0400

    changes

---
+++ src/components/JapaneseSentence.vue
@@ -0,0 +1,27 @@
+<template lang='pug'>
+div.japanese-sentence__wrapper
+    div.japanese-sentence
+        HiddenSentence(sentence='This is a big sentence This is a big sentence This is a big sentence This is a big sentence This is a big sentence This is a big sentence This is a big sentence This is a big sentence This is a big sentence This is a big sentence This is a big sentence ')
+</template>
+
+<script>
+export default {
+  name: 'JapaneseSentence',
+  components: {
+    HiddenSentence: () => import('./HiddenSentence')
+  }
+}
+</script>
+
+<style lang='scss'>
+.japanese-sentence__wrapper{
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    width: 100vw;
+}
+.japanese-sentence{
+    max-width: 30vw;
+}
+</style>
